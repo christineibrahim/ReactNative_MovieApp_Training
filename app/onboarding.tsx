e2dd25605d9041abc798f214d2cd69fcfe3345b5
christineibrahim <53993410+christineibrahim@users.noreply.github.com>
2025-08-26 14:06:48 +0300

    Add files via upload

---
+++ app/onboarding.tsx
@@ -0,0 +1,11 @@
+import {Text, View} from 'react-native';
+
+function Onboarding(props) {
+    return (
+        <View className="flex-1 justify-center items-center">
+            <Text>onboarding</Text>
+        </View>
+    );
+}
+
+export default Onboarding;
